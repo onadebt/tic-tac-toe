f09356f4ee4b432ceb3deb89adb9dcc6f0d65918
Jakub Španger <jakubspanger1025@gmail.com>
2025-03-11 14:31:30 +0100

    inject ElementSupplier in GamePhaseFactory

--- src/main/java/cz/muni/fi/pv260/team4/tictactoe/phase/GamePhaseFactory.java
+++ src/main/java/cz/muni/fi/pv260/team4/tictactoe/phase/GamePhaseFactory.java
@@ -1,7 +1,7 @@
 package cz.muni.fi.pv260.team4.tictactoe.phase;
 
 import cz.muni.fi.pv260.team4.tictactoe.board.BoardFactory;
-import cz.muni.fi.pv260.team4.tictactoe.element.AlphabeticElementSupplier;
+import cz.muni.fi.pv260.team4.tictactoe.element.ElementSupplier;
 import cz.muni.fi.pv260.team4.tictactoe.entity.MatchConfiguration;
 import cz.muni.fi.pv260.team4.tictactoe.interfaces.IOProvider;
 import lombok.AllArgsConstructor;
@@ -25,13 +25,14 @@ public class GamePhaseFactory {
      * Construct match phase.
      *
      * @param configuration Configuration of game match
+     * @param elementSupplier Element supplier for the board
      * @return match game phase
      */
-    public GamePhase getMatchPhase(final MatchConfiguration configuration) {
+    public GamePhase getMatchPhase(final MatchConfiguration configuration, final ElementSupplier elementSupplier) {
         return new MatchPhase(
                 this,
                 ioProvider,
                 configuration,
-                boardFactory.createEmptyBoard(configuration, new AlphabeticElementSupplier()));
+                boardFactory.createEmptyBoard(configuration, elementSupplier));
     }
 }
