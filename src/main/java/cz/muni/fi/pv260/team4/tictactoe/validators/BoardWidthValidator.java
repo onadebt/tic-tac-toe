1793baff2a960ae4d735fbb35a75d65b5852f8fc
Ivan <567817@muni.cz>
2025-03-12 20:26:48 +0100

    added max board sizes constraints

--- src/main/java/cz/muni/fi/pv260/team4/tictactoe/validators/BoardWidthValidator.java
+++ src/main/java/cz/muni/fi/pv260/team4/tictactoe/validators/BoardWidthValidator.java
@@ -8,7 +8,11 @@ public final class BoardWidthValidator implements InputValidator<Long> {
     @Override
     public Optional<String> validate(final Long widthOfTheBoard) {
         if (widthOfTheBoard < Const.MINIMAL_BOARD_WIDTH) {
-            return Optional.of("Minimum width of the board is 3");
+            return Optional.of("Minimum width of the board is " + Const.MINIMAL_BOARD_WIDTH);
+        }
+
+        if (widthOfTheBoard > Const.MAXIMAL_BOARD_WIDTH) {
+            return Optional.of("Maximum width of the board is " + Const.MAXIMAL_BOARD_WIDTH);
         }
 
         return Optional.empty();
