ef821e9ccf3daacb44b2fe5421b93a276f5bbb9f
Ivan <567817@muni.cz>
2025-03-12 20:33:45 +0100

    added WinningSequenceLength constraints and tests

--- src/main/java/cz/muni/fi/pv260/team4/tictactoe/validators/WinningSequenceLengthValidator.java
+++ src/main/java/cz/muni/fi/pv260/team4/tictactoe/validators/WinningSequenceLengthValidator.java
@@ -5,12 +5,25 @@ import cz.muni.fi.pv260.team4.tictactoe.Const;
 import java.util.Optional;
 
 public final class WinningSequenceLengthValidator implements InputValidator<Long> {
+    private final Long boardWidth;
+    private final Long boardHeight;
+
+    public WinningSequenceLengthValidator(Long boardWidth, Long boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+    }
+
     @Override
     public Optional<String> validate(final Long lengthOfTheWinningSequence) {
         if (lengthOfTheWinningSequence < Const.MINIMAL_LENGTH_OF_WINNING_SEQUENCE) {
-            return Optional.of("Length of the winning sequence has to be at least 3");
+            return Optional.of("Length of the winning sequence has to be at least " + Const.MINIMAL_LENGTH_OF_WINNING_SEQUENCE);
+        }
+
+        if (lengthOfTheWinningSequence > Math.min(boardWidth, boardHeight)) {
+            return Optional.of("Winning sequence length cannot exceed one of the board's dimensions");
         }
 
         return Optional.empty();
     }
 }
+
--- src/test/java/cz/muni/fi/pv260/team4/tictactoe/validators/WinningSequenceLengthValidatorTest.java
+++ src/test/java/cz/muni/fi/pv260/team4/tictactoe/validators/WinningSequenceLengthValidatorTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WinningSequenceLengthValidatorTest {
+    private final long boardWidth = 5L;
+    private final long boardHeight = 5L;
 
     @Test
     void shouldAllowAboveMinimalWinningSequenceLength() {
-        WinningSequenceLengthValidator validator = new WinningSequenceLengthValidator();
+        WinningSequenceLengthValidator validator = new WinningSequenceLengthValidator(boardWidth, boardHeight);
         assertTrue(validator.validate(Const.MINIMAL_LENGTH_OF_WINNING_SEQUENCE).isEmpty());
     }
 
-
     @Test
     void shouldRejectBelowMinimalWinningSequenceLength() {
-        WinningSequenceLengthValidator validator = new WinningSequenceLengthValidator();
+        WinningSequenceLengthValidator validator = new WinningSequenceLengthValidator(boardWidth, boardHeight);
         assertTrue(validator.validate(Const.MINIMAL_LENGTH_OF_WINNING_SEQUENCE - 1).isPresent());
+    }
+
+    @Test
+    void shouldRejectAboveMaximalWinningSequenceLength() {
+        WinningSequenceLengthValidator validator = new WinningSequenceLengthValidator(boardWidth, boardHeight);
+        assertTrue(validator.validate(Math.min(boardWidth, boardHeight) + 1).isPresent());
     }
 }
