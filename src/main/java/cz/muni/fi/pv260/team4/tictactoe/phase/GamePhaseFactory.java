788147de332170377014c1c2b11658a44ba467f4
Ivan <567817@muni.cz>
2025-03-15 13:45:02 +0100

    GamePhaseFactory extended with new functionality

--- src/main/java/cz/muni/fi/pv260/team4/tictactoe/phase/GamePhaseFactory.java
+++ src/main/java/cz/muni/fi/pv260/team4/tictactoe/phase/GamePhaseFactory.java
@@ -3,15 +3,33 @@ package cz.muni.fi.pv260.team4.tictactoe.phase;
 import cz.muni.fi.pv260.team4.tictactoe.board.BoardFactory;
 import cz.muni.fi.pv260.team4.tictactoe.element.ElementSupplier;
 import cz.muni.fi.pv260.team4.tictactoe.entity.MatchConfiguration;
+import cz.muni.fi.pv260.team4.tictactoe.infrastructure.TerminalBoardDisplay;
+import cz.muni.fi.pv260.team4.tictactoe.interfaces.BoardDisplay;
 import cz.muni.fi.pv260.team4.tictactoe.interfaces.IOProvider;
-import lombok.AllArgsConstructor;
+import cz.muni.fi.pv260.team4.tictactoe.movestrategy.StrategyFactory;
 
-@AllArgsConstructor
 public class GamePhaseFactory {
     private final IOProvider ioProvider;
-    private final ElementSupplier supplier;
+    private final ElementSupplier elementSupplier;
+    private final BoardFactory boardFactory;
+    private final StrategyFactory strategyFactory;
+    private final BoardDisplay boardDisplay;
 
-    private final BoardFactory boardFactory = new BoardFactory();
+    /**
+     * Constructor for GamePhaseFactory.
+     *
+     * @param provider IOProvider
+     * @param supplier   Element supplier
+     */
+    public GamePhaseFactory(final IOProvider provider, final ElementSupplier supplier) {
+        this.ioProvider = provider;
+        this.elementSupplier = supplier;
+        boardFactory = new BoardFactory();
+        strategyFactory = new StrategyFactory(provider);
+        boardDisplay = new TerminalBoardDisplay(provider);
+
+    }
+
 
     /**
      * Construct setup phase.
@@ -19,21 +37,24 @@ public class GamePhaseFactory {
      * @return setup game phase
      */
     public GamePhase getSetupPhase() {
-        return new SetupPhase(ioProvider, this, supplier);
+        return new SetupPhase(ioProvider, this, elementSupplier);
     }
 
     /**
      * Construct match phase.
      *
-     * @param configuration Configuration of game match
-     * @param elementSupplier Element supplier for the board
+     * @param configuration   Configuration of game match
+     * @param supplier Element supplier for the board
      * @return match game phase
      */
-    public GamePhase getMatchPhase(final MatchConfiguration configuration, final ElementSupplier elementSupplier) {
+    public GamePhase getMatchPhase(final MatchConfiguration configuration, final ElementSupplier supplier) {
         return new MatchPhase(
-                this,
                 ioProvider,
                 configuration,
-                boardFactory.createEmptyBoard(configuration, elementSupplier));
+                boardFactory.createEmptyBoard(configuration, supplier),
+                strategyFactory,
+                boardDisplay,
+                supplier
+        );
     }
 }
