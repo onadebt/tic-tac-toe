6312d6b0addadadde85dbd7d666ef8571a016a9f
Ivan <567817@muni.cz>
2025-03-15 18:14:55 +0100

    StrategyFactoryTest todo created

--- src/test/java/cz/muni/fi/pv260/team4/tictactoe/movestrategy/StrategyFactoryTest.java
+++ src/test/java/cz/muni/fi/pv260/team4/tictactoe/movestrategy/StrategyFactoryTest.java
@@ -1,36 +1,34 @@
 package cz.muni.fi.pv260.team4.tictactoe.movestrategy;
 
+import cz.muni.fi.pv260.team4.tictactoe.entity.MatchConfiguration;
 import cz.muni.fi.pv260.team4.tictactoe.interfaces.IOProvider;
 import cz.muni.fi.pv260.team4.tictactoe.movestrategy.enums.MoveStrategyEnum;
+import kotlin.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class StrategyFactoryTest {
     private IOProvider ioProvider;
     private StrategyFactory strategyFactory;
 
+    @Mock
+    private MatchConfiguration configuration;
+
     @BeforeEach
     void setUp() {
         ioProvider = mock(IOProvider.class);
-        strategyFactory = new StrategyFactory(ioProvider);
+        strategyFactory = new StrategyFactory(ioProvider, configuration);
     }
 
     @Test
     void testChooseSingleMoveStrategy() {
-        MoveStrategy strategy = strategyFactory.chooseStrategy(MoveStrategyEnum.SINGLE_MOVE);
+        MoveStrategy<Pair<Integer, Integer>> strategy = strategyFactory.chooseStrategy(MoveStrategyEnum.SINGLE_MOVE);
         assertInstanceOf(SingleMoveStrategy.class, strategy);
     }
 
-    @Test
-    void testChooseInvalidStrategy() {
-        try {
-            strategyFactory.chooseStrategy(MoveStrategyEnum.RANDOM_INVALID_MOVE);
-        } catch (IllegalArgumentException e) {
-            assertTrue(e.getMessage().contains("Invalid move type"));
-        }
-    }
+    //todo add test for invalid strategy
 }
