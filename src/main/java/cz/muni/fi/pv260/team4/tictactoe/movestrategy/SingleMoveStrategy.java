279497d542044fb1e6916a5f6c4ea876f015623b
Vít Falta <524987@muni.cz>
2025-04-09 14:21:37 +0200

    feat: simplify SingleMoveStrategy to not use MoveParameterGatherer

--- src/main/java/cz/muni/fi/pv260/team4/tictactoe/movestrategy/SingleMoveStrategy.java
+++ src/main/java/cz/muni/fi/pv260/team4/tictactoe/movestrategy/SingleMoveStrategy.java
@@ -3,6 +3,8 @@ package cz.muni.fi.pv260.team4.tictactoe.movestrategy;
 import cz.muni.fi.pv260.team4.tictactoe.board.Board;
 import cz.muni.fi.pv260.team4.tictactoe.entity.MatchConfiguration;
 import cz.muni.fi.pv260.team4.tictactoe.interfaces.IOProvider;
+import cz.muni.fi.pv260.team4.tictactoe.validators.ColumnBoundsValidator;
+import cz.muni.fi.pv260.team4.tictactoe.validators.RowBoundsValidator;
 import kotlin.Pair;
 import lombok.AllArgsConstructor;
 
@@ -19,19 +21,35 @@ public final class SingleMoveStrategy implements MoveStrategy<Pair<Integer, Inte
      */
     @Override
     public void executeMove(final Board board, final Character player) {
-        Pair<Integer, Integer> positions = getMoveParameterGatherer().gatherMoveParameters();
-        int row = positions.component1();
-        int col = positions.component2();
+        var position = askRowColumn(board);
+
+        board.setCell(position.getFirst() - 1, position.getSecond() - 1, player);
+    }
+
+    private Pair<Integer, Integer> askRowColumn(final Board board) {
+        int row = askRow();
+        int col = askColumn();
 
         while (!board.isCellEmpty(row - 1, col - 1)) {
-            System.err.println("This cell is already taken. Please choose another one.");
-            System.err.println();
-            positions = getMoveParameterGatherer().gatherMoveParameters();
-            row = positions.component1();
-            col = positions.component2();
+            ioProvider.writeError("This cell is already taken. Please choose another one.");
+            ioProvider.writeError("");
+            row = askRow();
+            col = askColumn();
         }
 
-        board.setCell(row - 1, col - 1, player);
+        return new Pair<>(row, col);
+    }
+
+    private int askColumn() {
+        return this.ioProvider.readInt(
+                "Enter column: ", new ColumnBoundsValidator(configuration.getBoardWidth())
+        );
+    }
+
+    private int askRow() {
+        return this.ioProvider.readInt(
+                "Enter row: ", new RowBoundsValidator(configuration.getBoardHeight())
+        );
     }
 
     /**
@@ -41,7 +59,7 @@ public final class SingleMoveStrategy implements MoveStrategy<Pair<Integer, Inte
      */
     @Override
     public MoveParameterGatherer<Pair<Integer, Integer>> getMoveParameterGatherer() {
-        return new PositionGatherer(ioProvider, configuration);
+        return null;
     }
 
     @Override
